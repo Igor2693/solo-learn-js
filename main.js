b0d3cc3a4057c5dd32e4786134fd195db2e4cc66
Igor2693 <i.bugaytsov@mail.ru>
2024-09-12 19:17:52 +0300

    выполнение упражнения 02

---
+++ main.js
@@ -0,0 +1,18 @@
+const title = 'Изучение JS'
+const screens = 'Простые, Сложные, Интерактивные'
+const screenPrice = 10
+const rollback = 10
+const fullPrice = 50000
+const adaptive = true
+
+console.log(typeof title)
+console.log(typeof fullPrice)
+console.log(typeof adaptive)
+
+console.log(screens.length)
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани \n' + 'Стоимость разработки сайта ' + fullPrice + 'рублей/долларов/гривен/юани')
+
+console.log(Array(screens.toLowerCase()))
+
+console.log('Процент отката посреднику за работу ' + fullPrice * (rollback / 100))
