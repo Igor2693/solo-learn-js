d77b9734618bd7c3d1cd50c4a1fd122d84e057ef
Igor Bugaytsov <i.bugaytsov@mail.ru>
2024-11-25 22:47:37 +0300

    25 ноября

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-const title = 'Изучение JS'
-const screens = 'Простые, Сложные, Интерактивные'
-const screenPrice = 10
+const title = "solo-learn-js"
+const screens = "Простые, Сложные, Интерактивные"
+const screenPrise = 100
 const rollback = 10
 const fullPrice = 50000
 const adaptive = true
 
-console.log(typeof title)
-console.log(typeof fullPrice)
-console.log(typeof adaptive)
+console.log(title)
+console.log(fullPrice)
+console.log(adaptive)
 
 console.log(screens.length)
 
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани \n' + 'Стоимость разработки сайта ' + fullPrice + 'рублей/долларов/гривен/юани')
+console.log("Стоимость верстки экратов " + screenPrise + " рублей/долларов/юаней")
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/юаней")
 
-console.log(Array(screens.toLowerCase()))
+console.log(screens.toLocaleLowerCase().split())
 
-console.log('Процент отката посреднику за работу ' + fullPrice * (rollback / 100))
-
-console.log(Number.isSafeInteger(fullPrice))
-console.log(Number.isSafeInteger(Infinity))
+console.log("Процент отката посрелнику за работу " + fullPrice * (rollback / 100))
