f16496e49840afaf037adfb7c9777cc946b2a594
Igor Bugaytsov <i.bugaytsov@mail.ru>
2024-11-26 20:55:38 +0300

    Выполнение упражнения 2

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
-const title = "solo-learn-js"
-const screens = "Простые, Сложные, Интерактивные"
-const screenPrise = 100
+'use strict'
+const title = prompt("Как называется ваш проект?")
+const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные")
+const screenPrise = +prompt("Сколько будет стоить данная работа?", "12000")
 const rollback = 10
-const fullPrice = 50000
-const adaptive = true
+
+const adaptive = confirm("Нужен ли адаптив на сайте?")
+const service1 = prompt("Какой дополнительный тип услуги нужен")
+const servicePrice1 = +prompt("Сколько это будет стоить?")
+const service2 = prompt("Какой дополнительный тип услуги нужен")
+const servicePrice2 = +prompt("Сколько это будет стоить?")
+const fullPrice = screenPrise + servicePrice1 + servicePrice2
+const servicePercentPrice = fullPrice - (fullPrice * (rollback / 100))
+
+if (fullPrice > 30000) {
+    console.log("Даем скидку 10%")
+} else if (fullPrice > 15000 && fullPrice <= 30000) {
+    console.log("Даем скидку в 5%")
+} else if (fullPrice <= 15000 && fullPrice >= 0) {
+    console.log("Скидка не предусмотрена")
+} else if (fullPrice < 0) {
+    console.log("Что-то пошло не так :(")
+}
+
 
 console.log(title)
 console.log(fullPrice)
 console.log(adaptive)
-
 console.log(screens.length)
-
 console.log("Стоимость верстки экратов " + screenPrise + " рублей/долларов/юаней")
 console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/юаней")
-
 console.log(screens.toLocaleLowerCase().split())
-
-console.log("Процент отката посрелнику за работу " + fullPrice * (rollback / 100))
+console.log(servicePercentPrice)
+console.log(Math.ceil(servicePercentPrice))
