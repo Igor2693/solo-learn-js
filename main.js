894f2497215ac184f1870e29b096727d62f9ab78
Igor Bugaytsov <i.bugaytsov@mail.ru>
2025-01-02 00:31:10 +0300

    Выполнение задания 10

--- main.js
+++ main.js
@@ -1,148 +1,44 @@
-'use strict';
+'use strict'
 
-const newTitle = document.getElementsByTagName('h1')[0]
-console.log(newTitle);
-
-const result = document.getElementsByClassName('handler_btn')[0]
-const reset = document.getElementsByClassName('handler_btn')[1]
-console.log(result);
-console.log(reset);
-
-const btnPlus = document.querySelector('.screen-btn')
-console.log(btnPlus);
-
-const itemsPercent = document.querySelectorAll('.other-items.percent')
-const itemsNumber = document.querySelectorAll('.other-items.number')
-console.log(itemsPercent);
-console.log(itemsNumber);
-
-const inputRoll = document.querySelector('.rollback input[type=range]')
-console.log(inputRoll);
-
-const spanValue = document.querySelector('.rollback .range-value')
-console.log(spanValue);
-
-const sumPrice = document.getElementsByClassName('total-input')[0]
-console.log(sumPrice);
-const sumScreens = document.getElementsByClassName('total-input')[1]
-console.log(sumScreens);
-const sumServes = document.getElementsByClassName('total-input')[2]
-console.log(sumServes);
-const sumResult = document.getElementsByClassName('total-input')[3]
-console.log(sumResult);
-const sumRollback = document.getElementsByClassName('total-input')[4]
-console.log(sumRollback);
-
-let blockScreens = document.querySelectorAll('.screen')
-console.log(blockScreens);
+const bookItems = document.querySelectorAll('.book')
+const bookWrapper = document.querySelectorAll('.books')
+const addBlock = document.querySelector('.adv')
+const bookFix = document.querySelectorAll('.book > h2')[4]
+const bookUlOne = document.querySelectorAll('.book > ul')[0]
+const bookLi = bookUlOne.getElementsByTagName('li')
+const bookUlTwo = document.querySelectorAll('.book > ul')[5]
+const bookLiTwo = bookUlTwo.getElementsByTagName('li')
+const newElemLi = document.createElement('li')
+const bookUlSix = document.querySelectorAll('.book > ul')[2]
+const bookLiSix = bookUlSix.getElementsByTagName('li')
 
 
 
+bookItems[0].before(bookItems[1])
+bookWrapper[0].append(bookItems[2])
+bookItems[5].before(bookItems[3])
 
-// const appData = {
-//     rollback: 10,
-//     screenPrice: 0,
-//     services: {},
-//     titleProject: '',
-//     screens: [],
-//     adaptive: true,
-//     fullPrice: 0,
-//     allServicePrices: 0,
-//     servicePercentPrice: 0,
+addBlock.remove()
 
-//     start: function () {
-//         appData.asking()
-//         appData.addPrices()
-//         appData.getFullPrice();
-//         appData.getServicePercentPrices()
-//         appData.getTitle();
+bookFix.textContent = 'Книга 3. this и Прототипы Объектов'
+bookFix.style.color = 'darkkhaki'
 
-//         appData.logger()
-//     },
-//     isNumber: function (num) {
-//         return !isNaN(parseFloat(num)) && isFinite(num)
-//     },
+bookLi[9].after(bookLi[2])
+bookLi[2].after(bookLi[5])
+bookLi[4].before(bookLi[7])
 
-//     asking: function () {
-//         do {
-//             appData.titleProject = prompt('Как называется ваш проект?')
-//         } while (appData.isNumber(appData.titleProject))
+bookLiTwo[1].after(bookLiTwo[9])
+bookLiTwo[5].after(bookLiTwo[3])
+bookLiTwo[8].after(bookLiTwo[6])
 
-//         for (let i = 0; i < 2; i++) {
-//             let name
-//             do {
-//                 name = prompt('Какие типы экранов нужно разработать?')
-//             } while (appData.isNumber(name))
+newElemLi.textContent = 'Глава 8: За пределами ES6'
+bookUlSix.append(newElemLi)
+bookLiSix[8].after(bookLiSix[10])
 
-//             let price = 0
 
-//             do {
-//                 price = prompt('Сколько будет стоить данная работа?')
-//             } while (!appData.isNumber(price))
 
-//             appData.screens.push({ id: i, name: name, price: price })
 
-//         }
 
-//         for (let i = 0; i < 2; i++) {
-//             let name
-//             do {
-//                 name = prompt('Какой дополнительный тип услуги нужен?')
-//             } while (appData.isNumber(name))
-
-//             let price = 0
-//             do {
-//                 price = +prompt('Сколько это будет стоить?')
-//             } while (!appData.isNumber(price))
-
-//             appData.services[name] = +price
-//         }
-//         appData.adaptive = confirm('Нужен ли адаптив на сайте?')
-//     },
-//     addPrices: function () {
-//         for (let screen of appData.screens) {
-//             appData.screenPrice += +screen.price
-//         }
-
-//         for (let key in appData.services) {
-//             appData.allServicePrices += appData.services[key]
-//         }
-//     },
-
-//     getFullPrice: function () {
-//         appData.fullPrice = +appData.screenPrice + appData.allServicePrices
-//     },
-
-//     getServicePercentPrices: function () {
-//         appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
-//     },
-
-//     getTitle: function () {
-//         appData.title = appData.titleProject.trim()[0].toUpperCase() + appData.titleProject.trim().slice(1).toLocaleLowerCase();
-//     },
-
-//     getRollbackMessage: function () {
-//         if (appData.fullPrice >= 30000) {
-//             return 'Даем скидку в 10%'
-//         } else if (30000 >= appData.fullPrice && appData.fullPrice > 15000) {
-//             return 'Даем скидку в 5%'
-//         } else if (15000 >= appData.fullPrice && appData.fullPrice >= 0) {
-//             return 'Скидка не предусмотрена'
-//         } else {
-//             return 'Что то пошло не так'
-//         }
-//     },
-
-//     logger: function () {
-//         for (let key in appData) {
-//             console.log('Ключ:' + key + ' ' + 'Значение:' + appData[key])
-//         }
-//         console.log(appData.screens);
-//     },
-
-// }
-
-// appData.start()
 
 
 
