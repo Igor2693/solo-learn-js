80218f3b4a8a11b3423f9aeab278bd934594b980
Igor2693 <i.bugaytsov@mail.ru>
2025-01-06 23:30:17 +0300

    Выполнение упражнения 11

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+'use strict'
+
+const btn = document.querySelector('#btn')
+const text = document.querySelector('#text')
+const square = document.querySelector('#square')
+const ebtn = document.querySelector('#e_btn')
+const range = document.querySelector('#range')
+const rangeText = document.querySelector('#range-span')
+
+ebtn.style.display = 'none'
+
+const logger = function(event) {
+    square.style.backgroundColor = text.value
+}
+
+const rangeInput = function(event) {
+    rangeText.textContent = event.target.value
+    square.style.height = event.target.value + '%'
+    square.style.width = event.target.value + '%' 
+
+}
+
+btn.addEventListener('click', logger)
+range.addEventListener('input', rangeInput)
+range.addEventListener('change', rangeInput)
+
+console.log(range);
+console.log(rangeText);
 
 
 
